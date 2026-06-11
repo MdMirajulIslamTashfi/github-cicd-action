c8485ba8d8cd48721603a68015f7042f48513f07
Md. Mirajul Islam Tashfi <mirajul.tashfi12@gmail.com>
2026-06-11 16:21:15 +0600

    Initial Comment

---
+++ src/main/java/com/tashfi/github_cicd_action/controller/WebController.java
@@ -0,0 +1,13 @@
+package com.tashfi.github_cicd_action.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the GitLab Web Application";
+    }
+}
+
